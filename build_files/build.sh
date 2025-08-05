0b1ec4afa53ee1ed90cc75a5d74c8603126b7f83
lisabirb <93887296+lisabirb@users.noreply.github.com>
2025-08-05 08:12:46 -0600

    experiment

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,8 +6,8 @@ rpm -Uvh http://mirror.ppa.trinitydesktop.org/trinity/rpm/f42/trinity-r14/RPMS/n
 
 # Prepare directories
 mkdir -p /usr/lib/opt/trinity
-mkdir -p /var/opt
-ln -s /usr/lib/opt/trinity /var/opt/trinity
+mkdir -p /opt
+ln -s /usr/lib/opt/trinity /opt/trinity
 
 dnf -y group install base-x
 
