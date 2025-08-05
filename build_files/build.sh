4c069579a8f35102a24f35dc8d958850dfdd7871
lisabirb <93887296+lisabirb@users.noreply.github.com>
2025-08-05 07:46:47 -0600

    idk if this will work

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,6 +4,11 @@ set -ouex pipefail
 
 rpm -Uvh http://mirror.ppa.trinitydesktop.org/trinity/rpm/f42/trinity-r14/RPMS/noarch/trinity-repo-14.1.4-1.fc42.noarch.rpm
 
+# Prepare directories
+mkdir -p /usr/lib/opt/trinity
+mkdir -p /var/opt
+ln -s /usr/lib/opt/trinity /var/opt/trinity
+
 COPR_REPOS=(
 ublue-os/packages
 )
